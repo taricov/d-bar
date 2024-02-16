9db3898c43d9e9eeb4982049706da81f1621bc1d
TaricOv <tareq.ouf@gmail.com>
2024-02-16 04:11:05 +0200

    bar is workin fine . added label based on shortcuts . added styling for better experience

--- src/components/AppAuto.vue
+++ src/components/AppAuto.vue
@@ -4,23 +4,80 @@ import '../styles/main.css'
 import { useMagicKeys, whenever } from '@vueuse/core'
 import randoms from '~/logic/random-words'
 const showBar = ref<boolean>(false)
-const isBg = ref<boolean>(false)
+const isBg = ref<boolean>(true)
+const loading = ref<boolean>(false)
 const autoComplete = ref()
-const isTransition = ref<boolean>(false)
+const isTransition = ref<boolean>(true)
 const spotlight = ref<HTMLInputElement>()
+const queryLabel = ref<String | null>(null)
+// const queryKeys = ref<any>({
+//   goto: '/',
+//   new: '+',
+//   help: '?',
+// })
 const keys = useMagicKeys()
 whenever(keys['ctrl+/'], () => {
   showBar.value = !showBar.value
 })
 
-const matches = ref<string[]>([])
-const searchList = ref<string[]>(randoms)
+const matches = ref<{ [key: string]: string }>({})
+const searchList = ref<any>(randoms)
 const searchValue = ref<string>('')
-const onCommand = () => {
-  matches.value = []
-  console.log(searchValue.value, matches.value)
-  matches.value = searchList.value = searchList.value.filter(word => word.toLowerCase().includes(searchValue.value.toLowerCase()))
+const onSelect = () => {
+  // console.log(searchValue.value, matches.value)
 }
+const onSubmit = () => {
+  // console.log(searchValue.value, matches.value)
+}
+
+// const getKeyByValue = (object: { [x: string]: any }, value: string | null) => Object.keys(object).find(key => object[key] === value)
+
+// function getKeyByValue(object: { [x: string]: any; hasOwnProperty: (arg0: string) => any }, value: any) {
+//   for (const prop in object) {
+//     // eslint-disable-next-line no-prototype-builtins
+//     if (object.hasOwnProperty(prop)) {
+//       if (object[prop] === value)
+//         return prop
+//     }
+//   }
+// }
+const addLabel = () => {
+  const q = searchValue.value
+  if (q.length === 1 && q[0] === '/')
+    return 'go to'
+}
+const onQuery = () => {
+  const l = searchList.value
+  const label = addLabel()
+  matches.value = {}
+  loading.value = true
+  setTimeout(() => loading.value = false, 1000)
+  if (label?.length) {
+    // searchValue.value = searchValue.value.length > 1 ? searchValue.value.slice(1) : searchValue.value
+    console.log(searchValue.value)
+    if (label === 'go to') {
+      queryLabel.value = label
+
+      matches.value = searchValue.value.length === 1
+        ? l.routes
+        : l.filter((word: string) => {
+          console.log(searchValue.value.slice(1))
+          return word.toLowerCase().includes(searchValue.value.slice(1).toLowerCase())
+        })
+    }
+  }
+  if (!searchValue.value.length) {
+    queryLabel.value = null
+    matches.value = {}
+  }
+
+  // ).map(route => route.split('_').join(' '))
+  //   .filter((word) => {
+  //   console.log(word.toLowerCase(), q.toLowerCase())
+  //   return word.toLowerCase().includes(q.toLowerCase())
+  // }
+}
+
 onMounted(() => {
   autoComplete.value = 'fefefe'
   //   onCommand()
@@ -31,32 +88,67 @@ onMounted(() => {
 <template>
   <div
     id="dont_change_mystyles"
-    class="h-fit flex m-0 space-x-4 items-center justify-center rounded-lg  px-6 py-10 w-6/12 md:w-7/12 mx-auto fixed -top-[1200px] left-1/2 transform -translate-x-1/2 -translate-y-1/2  z-[100] "
-    :class="[{ 'top-1/2': showBar }, { ' bg-violet-500 backdrop-blur-[3px] bg-opacity-5 shadow-lg': isBg }, { 'transition-all duration-300': isTransition }]"
+    class="h-fit flex m-0 space-x-4 items-center justify-center rounded-lg px-6 py-10 w-6/12 md:w-7/12 mx-auto fixed -top-[1200px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[100]"
+    :class="[
+      { 'top-1/2': showBar },
+      { 'transition-all duration-300': isTransition },
+    ]"
   >
-    <div class="flex-col overflow-hidden shd grow rounded transition duration-150 min-h-xs max-h-sm bg-gray-600 border-2 border-gray-400">
+    <div
+      :class="{ ' bg-violet-500 backdrop-blur-[3px] bg-opacity-5 shadow-lg fixed w-screen h-screen': isBg }" @click="showBar = false"
+    />
+    <div
+      class="z-1 flex-col overflow-hidden shd grow rounded transition duration-150 max-h-sm bg-gray-600 border-2 border-gray-400"
+    >
       <!-- <span class="w-11/12 border-b border-gray-900" /> -->
-      <form class="relative flex w-full h-full">
-        <input
-          ref="spotlight"
-          v-model="searchValue"
-          type="text"
-          placeholder="Command here.."
-          class="relative !font-semibold grow outline-none m-0 px-3 py-2 h-15 !text-xl block  !text-gray-300 bg-transparent transition duration-150 !border-none"
-          @input="onCommand"
-        >
-        <span ref="autoComplete" class="absolute flex items-center justify-start top-1/2 px-3 !left-0 transform -translate-y-1/2 font-semibold outline-none h-15 text-xl text-gray-300/30 bg-transparent transition duration-150">{{ "" }}</span>
+      <form class="relative flex w-full h-full" @submit.prevent="onSubmit">
+        <div v-if="loading" class="loader" />
+        <div v-if="queryLabel" class="w-20 special-label-container bg-sky-600 px-3 flex justify-center items-center">
+          <div class="special-label">
+            {{ queryLabel }}
+          </div>
+        </div>
+        <div class="w-full flex">
+          <input
+            ref="spotlight"
+            v-model="searchValue"
+            type="text"
+            placeholder="Command here.."
+            class="relative !font-semibold grow outline-none m-0 px-3 py-2 h-11 !text-xl block !text-gray-300 bg-transparent transition duration-150 !border-none"
+            @input="onQuery"
+          >
+        </div>
+        <span
+          ref="autoComplete"
+          class="absolute flex items-center justify-start top-1/2 px-3 !left-0 transform -translate-y-1/2 font-semibold outline-none h-15 text-xl text-gray-300/30 bg-transparent transition duration-150"
+        >{{ "" }}</span>
       </form>
       <hr class="border-t-light-50/20 border-b-none m-0">
       <!-- <hr class="!border-gray-100 opacity-40  m-auto"> -->
-      <div class="scroll-m-0 overflow-y-scroll w-full flex-1 h-[17rem]">
+      <div
+        :class="{ hidden: !Object.keys(matches).length }"
+        class="scroll-m-0 overflow-y-scroll w-full flex-1 h-[17rem]"
+      >
         <ul class="text-gray-200 h-full my-0 p-0">
-          <li v-for="match in matches" :key="match" class="hover:bg-gray-100 hover:bg-opacity-10 list-none text-left transition duration-200">
+          <li
+            v-for="(v, k) in matches"
+            :key="k"
+            :data-val="v"
+            tabindex="0"
+            class="hover:bg-gray-100 hover:bg-opacity-10 list-none text-left transition duration-200"
+          >
             <div
-              class="relative px-3 py-3 cursor-pointer after:content-[''] after:hidden after:w-1  after:h-full after:absolute after:top-0
-            after:rounded-tr after:rounded-br after:left-0 after:bg-gray-300 hover:after:flex"
+              :data-val="v"
+              class="relative px-3 py-3 cursor-pointer after:content-[''] after:hidden after:w-1 after:h-full after:absolute after:top-0 after:rounded-tr after:rounded-br after:left-0 after:bg-gray-300 hover:after:flex"
+              @click="onSelect"
             >
-              {{ match }}
+              {{ (`${k}`)
+                .split("_")
+                .join(" ")
+                .toLowerCase()
+                .split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+                .join(" ") }}
             </div>
           </li>
         </ul>
@@ -65,3 +157,40 @@ onMounted(() => {
     <!-- <AppDial /> -->
   </div>
 </template>
+
+<style>
+.special-label-container {
+  /* position: fixed;
+    top: 65px;
+    left: 45px;
+    padding: 3px; */
+    width: 20%;
+
+  }
+  .special-label {
+    /* border-radius: 4px;
+    padding: 3px;*/
+    text-transform: uppercase;
+    color: #ddd;
+    font-size: 15px;
+    font-weight: 600;
+    /* background: #c1ccca45; */
+}
+.loader {
+  position: fixed;
+    top: 60px;
+    right: 45px;
+    width: 25px;
+    padding: 5px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background: #c1ccca;
+    --_m: conic-gradient(#0000 10%,#000), linear-gradient(#000 0 0) content-box;
+    -webkit-mask: var(--_m);
+    mask: var(--_m);
+    -webkit-mask-composite: source-out;
+    mask-composite: subtract;
+    animation: l3 1s infinite linear;
+}
+@keyframes l3 {to{transform: rotate(1turn)}}
+</style>
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -23,60 +23,112 @@ const openOptionsPage = () => {
 
 <template>
   <main class="w-[500px] px-4 py-5 text-center bg-gray-900">
-    <div class="group flex items-center w-fit hover:bg-gray-100 hover:bg-opacity-10 pl-[5px] pr-4 py-[5px] font-semibold rounded transition-all duration-300 cursor-pointer dark:bg-blue-200 text-gray-400 mb-1">
+    <div
+      class="group flex items-center w-fit hover:bg-gray-100 hover:bg-opacity-10 pl-[5px] pr-4 py-[5px] font-semibold rounded transition-all duration-300 cursor-pointer dark:bg-blue-200 text-gray-400 mb-1"
+    >
       <a href="https://google.com" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 21 21"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="m10.5 14.5l-3-3l3-3m-3 3h11" /><path d="M16.5 9.5V5.495a1 1 0 0 0-.883-.993l-.12-.007L13.5 4.5m-6 0l-1.998-.005a1 1 0 0 0-.995.881l-.007.12v10.997a1 1 0 0 0 1 1l10 .006a1 1 0 0 0 .994-.882l.006-.117V14" /><path d="M8.5 3.5h4a1 1 0 1 1 0 2h-4a1 1 0 1 1 0-2z" /></g></svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          viewBox="0 0 21 21"
+        >
+          <g
+            fill="none"
+            fill-rule="evenodd"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="m10.5 14.5l-3-3l3-3m-3 3h11" />
+            <path
+              d="M16.5 9.5V5.495a1 1 0 0 0-.883-.993l-.12-.007L13.5 4.5m-6 0l-1.998-.005a1 1 0 0 0-.995.881l-.007.12v10.997a1 1 0 0 0 1 1l10 .006a1 1 0 0 0 .994-.882l.006-.117V14"
+            />
+            <path d="M8.5 3.5h4a1 1 0 1 1 0 2h-4a1 1 0 1 1 0-2z" />
+          </g>
+        </svg>
       </a>
-      <span class="font-normal group-hover:opacity-100 opacity-0 transition-all duration-300 group-hover:translate-x-1 transform -translate-x-2 ">
+      <span
+        class="font-normal group-hover:opacity-100 opacity-0 transition-all duration-300 group-hover:translate-x-1 transform -translate-x-2"
+      >
         Visit the website
       </span>
     </div>
-    <p class="btn transition duration-300 text-transparent bg-clip-text bg-gradient-to-r from-emerald-200 to-sky-700">
+    <p
+      class="btn transition duration-300 text-transparent bg-clip-text bg-gradient-to-r from-emerald-200 to-sky-700"
+    >
       Stay organized
     </p>
-    <h1 class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 mb-4 text-4xl font-extrabold md:text-5xl lg:text-6xl dark:text-white">
-      D-Notes
-      <span class="bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-2">Free</span>
+    <h1
+      class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 mb-4 text-4xl font-extrabold md:text-5xl lg:text-6xl dark:text-white"
+    >
+      D-Bar
+      <span
+        class="bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-2"
+      >Free</span>
     </h1>
 
-    <p v-if="isConnected" class="mb-6 text-lg font-normal lg:text-xl sm:px-16 xl:px-48 text-gray-300">
-      D-Notes is a business utility app for taking notes per page in Daftra ERP. All notes are stored securely and can be accessed from the
-      <button class="underline transition-all duration-200 hover:text-sky-300" @click="openOptionsPage">
+    <p
+      v-if="isConnected"
+      class="mb-6 text-lg font-normal lg:text-xl sm:px-16 xl:px-48 text-gray-300"
+    >
+      D-Notes is a business utility app for taking notes per page in Daftra ERP. All notes
+      are stored securely and can be accessed from the
+      <button
+        class="underline transition-all duration-200 hover:text-sky-300"
+        @click="openOptionsPage"
+      >
         Dashboard
       </button>
       so you would never miss what's next.
     </p>
     <button
-      v-if="!isConnected" class="text-3xl text-sky-100 bg-gradient-to-r from-emerald-500 to-sky-500 background-animate rounded px-5 py-3 my-5 transition-all duration-150 font-bold hover:scale-102 transform" @click="openOptionsPage"
+      v-if="!isConnected"
+      class="text-3xl text-sky-100 bg-gradient-to-r from-emerald-500 to-sky-500 background-animate rounded px-5 py-3 my-5 transition-all duration-150 font-bold hover:scale-102 transform"
+      @click="openOptionsPage"
     >
       Connect Now
-      <svg class="w-7 inline-block fill-sky-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>open-in-new</title><path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg>
+      <svg
+        class="w-7 inline-block fill-sky-200"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <title>open-in-new</title>
+        <path
+          d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+        />
+      </svg>
     </button>
 
     <div class="flex items-center justify-center space-x-5 mt-2 text-gray-300">
       <div class="">
-        <span class="opacity-50">
-          Theme:
-        </span>
-        <span>
-          Dark
-        </span>
+        <span class="opacity-50"> Theme: </span>
+        <span> Dark </span>
       </div>
       <div class="">
-        <span class="opacity-50">
-          Status:
-        </span>
+        <span class="opacity-50"> Status: </span>
         <span :class="color">
           {{ isConnected ? "Active" : "Disconnected" }}
         </span>
       </div>
     </div>
-    <button class="btn my-2 text-xs transition duration-150 group" @click="openOptionsPage">
-      Developed by Taric Ov <span class="group-hover:translate-x-1 transform transition duration-300 inline-block">→</span>
+    <button
+      class="btn my-2 text-xs transition duration-150 group"
+      @click="openOptionsPage"
+    >
+      Developed by Taric Ov
+      <span
+        class="group-hover:translate-x-1 transform transition duration-300 inline-block"
+      >→</span>
     </button>
-    <p class="text-xs font-normal my-0 text-gray-300 opacity-80 hover:opacity-100 transition duration-150">
+    <p
+      class="text-xs font-normal my-0 text-gray-300 opacity-80 hover:opacity-100 transition duration-150"
+    >
       This tool is part of the large business toolkit
-      <a class="underline transition-all duration-200 hover:text-sky-300" href="https://dsuite.com/dnotes">
+      <a
+        class="underline transition-all duration-200 hover:text-sky-300"
+        href="https://dsuite.com/dnotes"
+      >
         D-Suite</a>
       that aims to improve the digital experience for businesses in MENA.
     </p>
@@ -85,20 +137,20 @@ const openOptionsPage = () => {
 
 <style scoped>
 .background-animate {
-    background-size: 400%;
+  background-size: 400%;
 
-    -webkit-animation: AnimationName 10s ease infinite;
-    -moz-animation: AnimationName 10s ease infinite;
-    animation: AnimationName 10s ease infinite;
-  }
+  -webkit-animation: AnimationName 10s ease infinite;
+  -moz-animation: AnimationName 10s ease infinite;
+  animation: AnimationName 10s ease infinite;
+}
 
-  @keyframes AnimationName {
-    0%,
-    100% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
+@keyframes AnimationName {
+  0%,
+  100% {
+    background-position: 0% 50%;
   }
+  50% {
+    background-position: 100% 50%;
+  }
+}
 </style>
